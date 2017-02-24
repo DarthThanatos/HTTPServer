b810f1d1aab77fd9f4b4530c77886b3b11507f40
Robert <bielas.robert95@gmail.com>
2017-02-24 09:15:55 +0100

    images

--- scripts/evCh.js
+++ scripts/evCh.js
@@ -2,13 +2,12 @@ function everythingChanged(button){
 	console.log(button.value);
 	if (button.value=="virgin"){
 		document.getElementById("h1").innerHTML = "Death and destruction";
-		document.getElementById("im").src = "../images/Mr Anderson.png";
-		document.getElementById("im").alt = "Krasu z Mr Andersonem";
+		document.getElementById("im").src = "../images/murphy.png";
 		button.value = "second-base";
 		document.querySelector("p").textContent = "Yo";
 	}else{
 		document.getElementById("h1").innerHTML = "Peace and love";
-		document.getElementById("im").src = "../images/murphy.png";
+		document.getElementById("im").src = "../images/Revan.png";
 		document.getElementById("im").alt = "Murphy";
 		button.value = "virgin";
 	}
